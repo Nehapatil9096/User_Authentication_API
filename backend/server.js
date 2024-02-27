3a56921d069903fb59b8e1b3d807524b408b19e4
b30wulffz <nehapatil9096@gmail.com>
2024-02-27 23:09:54 +0530

    deploy4

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import { app, server } from "./socket/socket.js";
+
+
+const app = express();
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -27,7 +30,7 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
 	connectToMongoDB();
 	console.log(`Server Running on port ${PORT}`);
 });
